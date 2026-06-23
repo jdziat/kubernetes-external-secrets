56ac548b225f6c26c72960fd4da4a7bfcb32a1e3
Jordan Dziat <jdziat@gmail.com>
2026-06-23 08:24:33 -0700

    chore: Node 24 baseline + slim to AWS-only backends
    - engines >=24, add .nvmrc=24, convert resolutions->overrides (npm idiom)
    - remove Vault/Azure/GCP/AliCloud/IBM/Akeyless backends + their tests + configs
    - drop 8 non-AWS dependencies; keep kv-backend base class (AWS backends extend it)
    - regenerate package-lock.json to lockfileVersion 3
    - installs + 73 tests pass on Node 24
    
    Gate: node-expert A / cto A- GO

--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const vault = require('node-vault')
 const kube = require('kubernetes-client')
 const KubeRequest = require('kubernetes-client/backends/request')
 const pino = require('pino')
@@ -9,20 +8,9 @@ const fs = require('fs')
 const path = require('path')
 
 const awsConfig = require('./aws-config')
-const azureConfig = require('./azure-config')
-const alicloudConfig = require('./alicloud-config')
-const gcpConfig = require('./gcp-config')
-const ibmcloudConfig = require('./ibmcloud-config')
-const akeylessConfig = require('./akeyless-config')
 const envConfig = require('./environment')
 const SecretsManagerBackend = require('../lib/backends/secrets-manager-backend')
 const SystemManagerBackend = require('../lib/backends/system-manager-backend')
-const VaultBackend = require('../lib/backends/vault-backend')
-const AzureKeyVaultBackend = require('../lib/backends/azure-keyvault-backend')
-const GCPSecretsManagerBackend = require('../lib/backends/gcp-secrets-manager-backend')
-const AliCloudSecretsManagerBackend = require('../lib/backends/alicloud-secrets-manager-backend')
-const IbmCloudSecretsManagerBackend = require('../lib/backends/ibmcloud-secrets-manager-backend')
-const AkeylessBackend = require('../lib/backends/akeyless-backend')
 
 // Get document, or throw exception on error
 // eslint-disable-next-line security/detect-non-literal-fs-filename
@@ -60,68 +48,11 @@ const systemManagerBackend = new SystemManagerBackend({
   assumeRole: awsConfig.assumeRole,
   logger
 })
-const vaultOptions = {
-  apiVersion: 'v1',
-  endpoint: envConfig.vaultEndpoint,
-  requestOptions: {
-    // When running vault in HA mode, you must follow redirects on PUT/POST/DELETE
-    // See: https://github.com/kr1sp1n/node-vault/issues/23
-    followAllRedirects: true
-  }
-}
-// Include the Vault Namespace header if we have provided it as an env var.
-// See: https://github.com/kr1sp1n/node-vault/pull/137#issuecomment-585309687
-if (envConfig.vaultNamespace) {
-  vaultOptions.requestOptions.headers = {
-    'X-VAULT-NAMESPACE': envConfig.vaultNamespace
-  }
-}
-const vaultFactory = () => vault(vaultOptions)
-
-// The Vault token is renewed only during polling, not asynchronously. The default tokenRenewThreshold
-// is three times larger than the pollerInterval so that the token is renewed before it
-// expires and with at least one remaining poll opportunty to retry renewal if it fails.
-const vaultTokenRenewThreshold = envConfig.vaultTokenRenewThreshold
-  ? Number(envConfig.vaultTokenRenewThreshold) : 3 * envConfig.pollerIntervalMilliseconds / 1000
-
-const vaultBackend = new VaultBackend({
-  vaultFactory: vaultFactory,
-  tokenRenewThreshold: vaultTokenRenewThreshold,
-  logger: logger,
-  defaultVaultMountPoint: envConfig.defaultVaultMountPoint,
-  defaultVaultRole: envConfig.defaultVaultRole
-})
-const azureKeyVaultBackend = new AzureKeyVaultBackend({
-  credential: azureConfig.azureKeyVault(),
-  logger
-})
-const gcpSecretsManagerBackend = new GCPSecretsManagerBackend({
-  client: gcpConfig.gcpSecretsManager(),
-  logger
-})
-const alicloudSecretsManagerBackend = new AliCloudSecretsManagerBackend({
-  credential: alicloudConfig.credential,
-  logger
-})
-const ibmcloudSecretsManagerBackend = new IbmCloudSecretsManagerBackend({
-  credential: ibmcloudConfig.credential,
-  logger
-})
-const akeylessBackend = new AkeylessBackend({
-  credential: akeylessConfig.credential,
-  logger
-})
 
 const backends = {
   // when adding a new backend, make sure to change the CRD property too
   secretsManager: secretsManagerBackend,
-  systemManager: systemManagerBackend,
-  vault: vaultBackend,
-  azureKeyVault: azureKeyVaultBackend,
-  gcpSecretsManager: gcpSecretsManagerBackend,
-  alicloudSecretsManager: alicloudSecretsManagerBackend,
-  ibmcloudSecretsManager: ibmcloudSecretsManagerBackend,
-  akeyless: akeylessBackend
+  systemManager: systemManagerBackend
 }
 
 // backwards compatibility
